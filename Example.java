93e22b70a0098b18b075b85b60aecc36f47f4870
hiroki-kyoto-su <g1445151@cc.kyoto-su.sc.jp>
2016-06-01 01:46:55 +0900

    new one

---
+++ Example.java
@@ -0,0 +1,11 @@
+public class Example extends Object
+{
+    /**
+    * GitHubの練習に使う
+    */
+    public void main(String[] arguments)
+    {
+        System.out.printLn("hell world");
+        return;
+    }
+}
